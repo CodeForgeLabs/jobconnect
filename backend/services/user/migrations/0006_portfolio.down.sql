4e8cc123a37ce2f4420a6a32609fc5282c0f1b7e
Nathan Fisseha <nathan12fisseha@gmail.com>
2026-03-24 11:56:29 +0300

    user(migrations): add portfolio tables

---
+++ backend/services/user/migrations/0006_portfolio.down.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_portfolio_media_item_sort;
+DROP INDEX IF EXISTS idx_portfolio_items_profile_visibility_sort;
+
+DROP TABLE IF EXISTS portfolio_tags;
+DROP TABLE IF EXISTS portfolio_media;
+DROP TABLE IF EXISTS portfolio_items;
