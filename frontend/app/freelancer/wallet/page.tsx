7b36528ae84d181c6d3db8ab1640c6cb9267c3c2
Natnael <natibir400@gmail.com>
2026-04-05 10:37:35 +0300

    initialized frontend

---
+++ frontend/app/freelancer/wallet/page.tsx
@@ -0,0 +1,11 @@
+
+const walletPage = () => {
+  return (
+    <div>
+      <h1>Wallet Page</h1>
+      {/* Add wallet details and transactions here */}
+    </div>
+  );
+};
+
+export default walletPage;
